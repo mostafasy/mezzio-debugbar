66767757f1136cc1b079718e56766133b910888c
mostafa.seef-aldeen <mostafa.seef-aldeen@interligent.com>
2023-08-15 09:12:37 +0200

    add debugBarMiddlewareFactory.

---
+++ src/DebugBarMiddlewareFactory.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+
+namespace Mezzio\DebugBar;
+
+use DebugBar\DebugBar;
+use Psr\Container\ContainerInterface;
+use Psr\Http\Message\ResponseFactoryInterface;
+use Psr\Http\Message\StreamFactoryInterface;
+
+class DebugBarMiddlewareFactory
+{
+    public function __invoke(ContainerInterface $container): DebugBarMiddleware
+    {
+
+        $responseFactory = $container->get(ResponseFactoryInterface::class);
+        $streamFactory = $container->get(StreamFactoryInterface::class);
+        $debugBar = $container->get( DebugBar::class);
+        $config = $container->get('config');
+        $debugbarConfig = $config['debugbar'] ?? [];
+
+        return new DebugBarMiddleware($debugBar,$responseFactory, $streamFactory,$debugbarConfig);
+    }
+}
