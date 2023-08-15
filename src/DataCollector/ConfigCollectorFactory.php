5dfaa8069b445d3c57e4e4ea819dff7081d74215
mostafa.seef-aldeen <mostafa.seef-aldeen@interligent.com>
2023-08-15 09:25:22 +0200

    add factory for configCollector.

---
+++ src/DataCollector/ConfigCollectorFactory.php
@@ -0,0 +1,17 @@
+<?php
+declare (strict_types=1);
+
+namespace Mezzio\DebugBar\DataCollector;
+
+use DebugBar\DataCollector\ConfigCollector;
+use Psr\Container\ContainerInterface;
+
+class ConfigCollectorFactory
+{
+    public function __invoke(ContainerInterface $container): ConfigCollector
+    {
+        $data = $container->get('config');
+
+        return new ConfigCollector($data);
+    }
+}
