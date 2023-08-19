8615085fa753eb617dd9e9d74e8c30dcd94ffe8f
mostafa.seef-aldeen <mostafa.seef-aldeen@interligent.com>
2023-08-19 13:12:21 +0200

    add FileStorgeFactory.

--- src/Storage/FileStorageFactory.php
+++ src/Storage/FileStorageFactory.php
@@ -4,15 +4,27 @@ declare(strict_types=1);
 
 namespace Mezzio\DebugBar\Storage;
 
+use DebugBar\DebugBarException;
 use DebugBar\Storage\FileStorage;
+use Psr\Container\ContainerExceptionInterface;
 use Psr\Container\ContainerInterface;
+use Psr\Container\NotFoundExceptionInterface;
 
 class FileStorageFactory
 {
+    /**
+     * @throws ContainerExceptionInterface
+     * @throws DebugBarException
+     * @throws NotFoundExceptionInterface
+     */
     public function __invoke(ContainerInterface $container): FileStorage
     {
-        $config  = $container->get('config');
-        $dirname = $config['storge_dir'] ?? 'data/debugbar';
+        $dirname = $container->get('config')['debugbar' ]['storge_dir'] ?? null;
+
+        if ($dirname === null) {
+                throw new DebugBarException("missing config:fileStorage needs storge_dir");
+        }
+
         return new FileStorage($dirname);
     }
 }
