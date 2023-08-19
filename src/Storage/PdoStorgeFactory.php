0a7a5d49b1db93db9f333766396ce33953a41695
mostafa.seef-aldeen <mostafa.seef-aldeen@interligent.com>
2023-08-19 13:18:09 +0200

    update PdoStorge Factory.

--- src/Storage/PdoStorgeFactory.php
+++ src/Storage/PdoStorgeFactory.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace Mezzio\DebugBar\Storage;
 
+use DebugBar\DebugBarException;
 use DebugBar\Storage\PdoStorage;
 use PDO;
+use PDOException;
 use Psr\Container\ContainerExceptionInterface;
 use Psr\Container\ContainerInterface;
 use Psr\Container\NotFoundExceptionInterface;
@@ -15,11 +17,19 @@ class PdoStorgeFactory
     /**
      * @throws ContainerExceptionInterface
      * @throws NotFoundExceptionInterface
+     * @throws DebugBarException
      */
     public function __invoke(ContainerInterface $container): PdoStorage
     {
-        $config = $container->get('config')[ 'debugbar' ][ 'pdo' ];
-        $pdo    = new PDO($config[ 'dsn' ], $config[ 'user' ], $config[ 'password' ]);
+        $config = $container->get('config')[ 'debugbar' ][ 'pdo' ] ?? null;
+        if ($config === null) {
+            throw new DebugBarException("missing config for Pdo");
+        }
+        try {
+            $pdo = new PDO($config[ 'dsn' ], $config[ 'user' ], $config[ 'password' ]);
+        } catch (PDOException $exception) {
+            throw new DebugBarException('pdo exception:' . $exception->getMessage());
+        }
         return new PdoStorage($pdo);
     }
 }
